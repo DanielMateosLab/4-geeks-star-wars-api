00c8371781d190aeb1a7fbc90f8d889ab1f4695d
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-29 14:34:46 +0000

    Fix typos in models and upgrade db

--- src/models.py
+++ src/models.py
@@ -3,13 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 planet_favourites = db.Table('planet_favourites',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), nullable=False, primary_key=True),
-    db.Column('planet_id', db.Integer, db.ForeignKey('planet.id'), nullable=False, primary_key=True)
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('planet_id', db.Integer, db.ForeignKey('planet.id'), primary_key=True)
 )
 
 people_favourites = db.Table('people_favourites',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), nullable=False, primary_key=True),
-    db.Column('people_id', db.Integer, db.ForeignKey('people.id', nullable=False), primary_key=True)
+    db.Column('people_id', db.Integer, db.ForeignKey('people.id'), nullable=False, primary_key=True)
 )
 
 class User(db.Model):
@@ -31,25 +31,26 @@ class User(db.Model):
         }
 
 class Planet(db.Model):
-    id = db.Column(Integer, primary_key=True)
-    name = db.Column(String(40), nullable=False, unique=True)
-    climate = db.Column(String(10), nullable=False)
-    terrain = db.Column(String(10), nullable=False)
-    diameter = db.Column(Integer, nullable=False)
-    gravity = db.Column(Integer, nullable=False)
-    orbital_period = db.Column(Integer, nullable=False)
-    population = db.Column(Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False, unique=True)
+    climate = db.Column(db.String(10), nullable=False)
+    terrain = db.Column(db.String(10), nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    gravity = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    population = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return self.name
 
 class People(db.Model):
-    id = Column(Integer, primary_key=True)
-    name = Column(String(40), nullable=False, unique=True)
-    birthyear = Column(Integer, nullable=False)
-    gender = Column(String(6), nullable=False)
-    height = Column(Integer, nullable=False)
-    homeplanet = Column(Integer, ForeignKey('planet.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False, unique=True)
+    birthyear = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(6), nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    # TODO: O:M relationship homeplanet = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+    
 
     def __repr__(self):
         return self.name
