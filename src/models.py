6d2f60139b1434d7064aae8cfc1b4e157a38fe1b
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-29 15:06:32 +0000

    Remove planet and people favourite backrefs

--- src/models.py
+++ src/models.py
@@ -16,10 +16,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    planet_favourites = db.relationship('Planet', secondary=planet_favourites, lazy='subquery',
-        backref=db.backref('favourite_of', lazy=True))
-    people_favourites = db.relationship('People', secondary=people_favourites, lazy='subquery',
-        backref=db.backref('favourite_of', lazy=True))
+    planet_favourites = db.relationship('Planet', secondary=planet_favourites, lazy='subquery')
+    people_favourites = db.relationship('People', secondary=people_favourites, lazy='subquery')
     
     def __repr__(self):
         return self.email
