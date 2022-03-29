31d65da1091e7373cde79e3a720f05d68af875f0
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-29 16:08:07 +0000

    Add endpoint specifications

--- src/main.py
+++ src/main.py
@@ -29,15 +29,61 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-    users = User.query.all()
-    
-    response_body = {
-        "users": [user.serialize() for user in users]
-    }
+# People endpoints
+@app.route('/people')
+def get_people():
+    "Returns the list of people"
+    pass
 
-    return jsonify(response_body), 200
+@app.route('/people/<int:person_id>')
+def get_person(person_id):
+    "Returns person with id = person_id"
+    pass
+
+# Planet endpoints
+@app.route('/planets')
+def get_planets():
+    "Returns the list of planets"
+    pass
+
+@app.route('/planets/<int:planet_id>')
+def get_planet(planet_id):
+    "Returns planet with id = planet_id"
+    pass
+
+# User endpoints
+@app.route('/users', methods=['GET'])
+def get_users():
+    "Returns the list of users"
+    users = [user.serialize() for user in User.query.all()]
+
+    return jsonify(users), 200
+
+@app.route('/users/<int:user_id>/favourites')
+def get_favourites(user_id):
+    "Returns favorites of user with id='user_id'"
+
+@app.route('/users/<int:user_id>/favourites', methods=['POST'])
+def add_favourite(user_id):
+    """
+    Adds favourite to user with 'user_id'
+    Assumes a request body of shape: 
+    {
+        "type": "planet" | "people",
+        "id": Int
+    }
+    """    
+    pass
+
+@app.route('/users/<int:user_id>/favourites/people/<int:person_id>', methods=['DELETE'])
+def delete_person_favourite(user_id, person_id):
+    "Deletes favorite person with id = person_id from user with id = user_id"
+    pass
+
+@app.route('/users/<int:user_id>/favourites/planet/<int:planet_id>', methods=['DELETE'])
+def delete_planet_favourite(user_id, planet_id):
+    "Deletes favorite person with id = planet_id from user with id = user_id"
+    pass
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
